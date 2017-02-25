5c301cf88d2559ed96c64192c2e8562c09ab5aa3
Abhishek Pasi <abhishekpasi97@gmail.com>
2017-02-25 16:59:33 +0530

    Delete q2_gifts.py

--- q2/q2_gifts.py
+++
@@ -1,8 +0,0 @@
-class Gift:
-	'Gift class for all essential gifts'
-
-	def __init__(self, name, price, value, type):
-		self.name = name
-		self.price = price
-		self.value = value
-		self.type = type
