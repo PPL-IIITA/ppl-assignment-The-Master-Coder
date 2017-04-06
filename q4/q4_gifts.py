23aef49ac8d9692dd843e7a2816295fc3967e27f
Abhishek Pasi <abhishekpasi97@gmail.com>
2017-04-06 17:47:56 +0530

    Delete q4_gifts.py

--- q4/q4_gifts.py
+++
@@ -1,9 +0,0 @@
-class Gift:
-	'gift class for all gifts'
-
-	def __init__(self, name, price, value, type):
-		'constructor'
-		self.name = name
-		self.price = price
-		self.value = value
-		self.type = type
