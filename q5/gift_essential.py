4d7b847e9fd6b8d78923798cbdc4cd767eaffbe5
Abhishek Pasi <abhishekpasi97@gmail.com>
2017-04-09 21:52:02 +0530

    Delete gift_essential.py

--- q5/gift_essential.py
+++
@@ -1,8 +0,0 @@
-from gifts import Gift
-
-class EssentialGift(Gift):
-	'gift class for essential gifts'
-
-	def __init__(self, name, price, value, type):
-		'constructor'
-		Gift.__init__(self, name, price, value, type)
