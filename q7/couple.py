bf3477039ec3f7786009d56b98a1cf54232bfd36
Abhishek Pasi <abhishekpasi97@gmail.com>
2017-04-09 21:59:14 +0530

    Add files via upload

---
+++ q7/couple.py
@@ -0,0 +1,26 @@
+class Couple:
+	'couple class for all couples'
+
+	def __init__(self, boy, girl):
+		'constructor'
+		self.boy = boy
+		'''@ivar: stores boy object of couple'''
+		self.girl = girl
+		'''@ivar: stores girl object of couple'''
+		self.happiness = 0
+		'''@ivar: happiness of couple'''
+		self.compatibility = 0
+		'''@ivar: compatibility of couple'''
+		self.GFT = []
+		'''@ivar: List to store all the gifts given, from boy to girl of a couple'''
+
+	def set_happiness(self):
+		'set the happiness of a couple'
+		self.happiness = self.boy.happiness + self.girl.happiness
+
+	def set_compatibility(self):
+		'set the compatibility of a couple'
+		a = self.boy.gfbudget - self.girl.mbudget
+		b = abs(self.boy.atr - self.girl.atr)
+		c = abs(self.boy.intelli - self.girl.intelli)
+		self.compatibility = a+b+c
